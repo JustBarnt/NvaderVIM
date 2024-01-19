922975c6b47a0f7d6a6c52d920fd49d1c8468b87
justbarnt <functnal@gmail.com>
2024-01-19 15:03:27 -0500

    fix: Need to trouble shoot issue with harpoon/telescope

--- lua/core/plugins/harpoon.lua
+++ lua/core/plugins/harpoon.lua
@@ -1,34 +1,33 @@
-local map = require 'core.utils'.map
-local harpoon = require 'harpoon'
-local conf = require 'telescope.config'.values
+local map = require("core.utils").map
+local harpoon = require("harpoon")
+local conf = require("telescope.config").values
 
 -- TODO: Handle user configuration
-local exist, user_config = pcall(require, 'user.config')
-local harpoon_config = exist and type(user_config) == 'table' and user_config.harpoon or {}
+local exist, user_config = pcall(require, "user.config")
+local harpoon_config = exist and type(user_config) == "table" and user_config.harpoon or {}
 
-local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
+harpoon:setup({ harpoon_config })
 
-    require('telescope.pickers').new({}, {
-        prompt_title = "⇁ Harpoon ↽",
-        finder = require('telescope.finders').new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({})
-    }):find()
+local keys = function()
+	-- Append current open file to our harpon list
+	map("n", "<leader>ha", function()
+		harpoon:list():append()
+		local file_name = vim.api.nvim_buf_get_name(0)
+		vim.notify("[Harpoon] Added " .. file_name .. " to list", vim.log.levels.INFO)
+	end, { desc = "Harpoon Add" })
+
+	-- Toggle a UI popup of or current harpooned items.
+	map("n", "<leader>hl", function()
+		harpoon.ui:toggle_quick_menu(harpoon:list())
+	end, { desc = "Harpoon Toggle" })
+
+	for i = 1, 5 do
+		map("n", string.format("<leader>%s", i), function()
+			harpoon:list():select(i)
+		end, { desc = "Harpoon jump to file: [" .. i .. "]" })
+	end
 end
-
-harpoon:setup({harpoon_config})
 
 local M = {}
-
-M.keys = function()
-    map('n', "<leader>ha", function() harpoon:list():append() end, { desc = "Harpoon Add" })
-    map('n', "<leader>hl", function() toggle_telescope(harpoon:list()) end, { desc = "Harpoon List" })
-end
-
+M.keys = keys
 return M
