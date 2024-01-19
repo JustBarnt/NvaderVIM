30f561d3838724f0e62698c1d858590367757424
justbarnt <functnal@gmail.com>
2024-01-19 14:54:38 -0500

    fix: Omnisharp LSP now works properly

--- lua/core/lsp/servers/omnisharp.lua
+++ lua/core/lsp/servers/omnisharp.lua
@@ -1,4 +1,6 @@
-local omnisharp_bin = vim.fn.stdpath("data") .. "\\mason\\packages\\omnisharp\\libexec\\Omnisharp.dll"
+local omnisharp_bin = vim.fn.stdpath("data") .. "\\mason\\packages\\omnisharp\\libexec\\Omnisharp.exe"
+local pid = vim.fn.getpid()
+
 --[[
     Make sure an .omnisharp.json file exists in ~./omnisharp/.omnisharp.json with at least the following snippets
     {
@@ -9,8 +11,8 @@ local omnisharp_bin = vim.fn.stdpath("data") .. "\\mason\\packages\\omnisharp\\l
 ]]
 
 return {
-    handlers = {
-        ["textDocument/definition"] = require('omnisharp_extended').handler,
-    },
-	cmd = { 'dotnet', omnisharp_bin, 'RoslynExtensionsOptions:EnableDecompilationSupport=true'},
+	handlers = {
+		["textDocument/definition"] = require("omnisharp_extended").handler,
+	},
+	cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid), 'RoslynExtensionsOptions:EnableDecompilationSupport=true' },
 }
