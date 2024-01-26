82811c5ad8259af3184000e0058ecf2f7ac1086e
justbarnt <functnal@gmail.com>
2024-01-26 16:28:27 -0500

    fix: temporarily reverted svlete lang

--- lua/core/lsp/servers/svelte.lua
+++ lua/core/lsp/servers/svelte.lua
@@ -1,6 +1,1 @@
-local util = require 'lspconfig.util'
-return {
-    cmd = { 'svelteserver', '--stdio' },
-    filetype = { 'svelte' },
-    root_dir = util.root_pattern("svelte.config.ts", "svelte.config.js")
-}
+
