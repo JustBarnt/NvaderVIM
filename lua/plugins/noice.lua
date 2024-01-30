b5c247ca8bf1ce00b70b8c196d1563074c4a8106
justbarnt <functnal@gmail.com>
2024-01-30 09:25:03 -0500

    change: removed nvim-notify. May change to fidget. Using "mini" from noice currently

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,12 +3,6 @@ return {
 	event = { "VeryLazy" },
 	dependencies = {
 		{ "MunifTanjim/nui.nvim" },
-		{
-			"rcarriga/nvim-notify",
-			config = function()
-				require("user.plugins.notify").setup()
-			end,
-		},
 	},
 	config = function()
 		require("user.plugins.noice").setup()
