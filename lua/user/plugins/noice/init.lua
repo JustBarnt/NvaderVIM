ee8ec6be3f7f04b5bebc9e23cc628acdaf22e595
justbarnt <functnal@gmail.com>
2024-01-30 09:57:50 -0500

    change: added route for lsp hover not showing "No information available"

--- lua/user/plugins/noice/init.lua
+++ lua/user/plugins/noice/init.lua
@@ -1,69 +1,73 @@
 local M = {}
-local noice = require("noice")
+local noice = require "noice"
 
 M.setup = function()
     noice.setup({
         routes = {
             { -- Disables showing confirm messages aka things like "saving" a buffer
-            filter = {
-                event = "msg_show",
-                kind = { "confirm", "confirm_sub", "wmsg", "search_count", "quickfix", "echo" },
+                filter = {
+                    event = "msg_show",
+                    kind = { "confirm", "confirm_sub", "wmsg", "search_count", "quickfix", "echo" },
+                },
+                opts = { skip = true },
             },
-            opts = { skip = true },
-        },
-    },
-    lsp = {
-        -- Note: Disabled because I overwrite [window/showMessage] to display my own LSP errors
-        hover = {
-            silent = true,
-        },
-        message = {
-            enabled = true,
-        },
-        override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-        },
-    },
-    presets = {
-        bottom_search = true,
-        command_palette = false,
-        lsp_doc_border = true,
-        inc_rename = false, -- Install inc_rename.nvim to use
-        long_message_to_split = true,
-    },
-    views = {
-        cmdline_popup = {
-            position = {
-                row = 5,
-                col = "50%",
-            },
-            size = {
-                width = 60,
-                height = "auto",
+            {
+                filter = { find = "No information available" },
+                opts = { stop = true },
             },
         },
-        popupmenu = {
-            relative = "editor",
-            position = {
-                row = 8,
-                col = "50%",
+        lsp = {
+            -- Note: Disabled because I overwrite [window/showMessage] to display my own LSP errors
+            hover = {
+                silent = true,
             },
-            size = {
-                width = 60,
-                height = 10,
+            message = {
+                enabled = true,
             },
-            border = {
-                style = "rounded",
-                padding = { 0, 1 },
-            },
-            win_options = {
-                winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
             },
         },
-    },
-})
+        presets = {
+            bottom_search = true,
+            command_palette = false,
+            lsp_doc_border = true,
+            inc_rename = false, -- Install inc_rename.nvim to use
+            long_message_to_split = true,
+        },
+        views = {
+            cmdline_popup = {
+                position = {
+                    row = 5,
+                    col = "50%",
+                },
+                size = {
+                    width = 60,
+                    height = "auto",
+                },
+            },
+            popupmenu = {
+                relative = "editor",
+                position = {
+                    row = 8,
+                    col = "50%",
+                },
+                size = {
+                    width = 60,
+                    height = 10,
+                },
+                border = {
+                    style = "rounded",
+                    padding = { 0, 1 },
+                },
+                win_options = {
+                    winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+                },
+            },
+        },
+    })
 end
 
 return M
