13f8f58b5e1a23c3e4b15e59b56e25f88faae2d1
justbarnt <functnal@gmail.com>
2024-01-30 12:31:25 -0500

    feat: added trouble, disabled fused_layout for telescope, currently not actually applying

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,16 +13,4 @@ return {
         require 'user.plugins.telescope'.setup.telescope.setup()
     end,
   },
-  --[[ {
-      "nvim-telescope/telescope-file-browser.nvim",
-      lazy = false,
-      dependencies = {
-          "nvim-telescope/telescope.nvim",
-          "nvim-lua/plenary.nvim"
-      },
-      config = function()
-          require 'user.plugins.telescope'.setup.file_browser.setup()
-          require 'user.plugins.telescope'.setup.file_browser.keys()
-      end,
-  } ]]
 }
