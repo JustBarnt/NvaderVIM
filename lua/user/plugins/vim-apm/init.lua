c7ce320f626980dddf6878c231215b9574edb79c
justbarnt <functnal@gmail.com>
2024-02-01 15:54:45 -0500

    Setup vim-apm

--- lua/user/plugins/vim-apm/init.lua
+++ lua/user/plugins/vim-apm/init.lua
@@ -3,10 +3,11 @@ local apm = require 'vim-apm'
 local map = require 'core.utils'.map
 
 M.setup = function()
-    apm:setup({})
-end
-
-M.keys = function()
+    apm:setup({
+        reporter = {
+            type = "memory"
+        }
+    })
     map('n', '<leader>apm', function() apm:toggle_monitor() end, { desc = "Vim Actions Per Minute"})
 end
 
