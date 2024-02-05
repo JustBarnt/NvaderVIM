6a7726475be9c8a7d1b86a2e6abeffe9229dc772
justbarnt <functnal@gmail.com>
2024-02-05 14:24:17 -0500

    fix: Fixed markdown files outside of doc files opening in a float

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -8,17 +8,20 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
     callback = function(event)
         local filetype = vim.bo[event.buf].filetype
 
-        -- Only run if the filetype is a help file
-        if filetype == "help" or filetype == "markdown" then
-            -- Get the newly opened help window
-            -- and attempt to open a Detour() float
-            local help_win = vim.api.nvim_get_current_win()
-            local ok = require("detour").Detour()
 
-            -- If we successfully create a float of the help file
-            -- Close the split
-            if ok then
-                vim.api.nvim_win_close(help_win, false)
+        if event.file:match("doc") ~= nil then
+            -- Only run if the filetype is a help file
+            if filetype == "help" or filetype == "markdown" then
+                -- Get the newly opened help window
+                -- and attempt to open a Detour() float
+                local help_win = vim.api.nvim_get_current_win()
+                local ok = require("detour").Detour()
+
+                -- If we successfully create a float of the help file
+                -- Close the split
+                if ok then
+                    vim.api.nvim_win_close(help_win, false)
+                end
             end
         end
     end,
