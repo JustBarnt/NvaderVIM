aac2b8b52ee2622fe4760f3fb11d242134d23ba5
justbarnt <functnal@gmail.com>
2024-02-08 12:29:20 -0500

    feat: added fidget, removed noice

---
+++ lua/plugins/fidget.lua
@@ -0,0 +1,11 @@
+return {
+    "j-hui/fidget.nvim",
+    --NOTE: Move opts to user config
+    opts = {
+        notification = {
+            window = {
+                align = "top",
+            },
+        },
+    },
+}
