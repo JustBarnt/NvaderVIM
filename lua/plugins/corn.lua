d3581d6f98e5b04f2bbcdf6284815e2ff498adb0
justbarnt <functnal@gmail.com>
2024-02-10 10:38:20 -0500

    chore: refactored corn

--- lua/plugins/corn.lua
+++ lua/plugins/corn.lua
@@ -1,11 +1,13 @@
 return {
     {
         "RaafatTurki/corn.nvim",
-        keys = function()
-            require("user.plugins.corn").keys()
-        end,
         config = function()
-            require("user.plugins.corn").setup()
+            local map = require("core.utils").map
+            require('corn').setup({})
+
+            -- Keys
+            map("n", "<leader>ct", "<CMD>CornToggle<CR>", { desc = "Toggle Corner Diagnostics" })
+            map("n", "<leader>cs", "<CMD>CornScopeCycle<CR>", { desc = "Cycle Scope Types" })
         end,
     },
 }
