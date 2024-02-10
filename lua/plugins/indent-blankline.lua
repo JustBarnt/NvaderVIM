bd2aa31bcf3e3d62faf6d49937a4388d27e6b4f6
justbarnt <functnal@gmail.com>
2024-02-10 10:42:33 -0500

    chore: refactored indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,6 +1,18 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
-        require 'user.plugins.indent-blankline'
+        local hooks = require "ibl.hooks"
+        local scope = "focus"
+        local indent = "passive"
+
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            vim.api.nvim_set_hl(0, "focus", { fg = "#C4A7E7" })
+            vim.api.nvim_set_hl(0, "passive", { fg = "#41425e" })
+        end)
+
+        require("ibl").setup({
+            scope = { highlight = scope },
+            indent = { highlight = indent },
+        })
     end,
 }
