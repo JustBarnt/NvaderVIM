0317d00a300d85036badf354490124061b8309ad
justbarnt <functnal@gmail.com>
2024-02-10 10:41:12 -0500

    chore: refactored harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,10 +2,31 @@ return {
     "theprimeagen/harpoon",
     branch = "harpoon2",
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
     },
     config = function()
-        require 'user.plugins.harpoon'
+        local map = require("core.utils").map
+        local config = require("user.plugins.harpoon").config
+        local harpoon = require "harpoon"
+    
+        harpoon:setup(config)
+
+        -- Keys
+        map("n", "<leader>ha", function()
+            harpoon:list():append()
+            local file_name = vim.api.nvim_buf_get_name(0)
+            vim.notify("[Harpoon] Added " .. file_name .. " to list", vim.log.levels.INFO)
+        end, { desc = "Harpoon Add" })
+
+        -- Toggle a UI popup of or current harpooned items.
+        map("n", "<leader>hl", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end, { desc = "Harpoon Toggle" })
+
+        for i = 1, 5 do
+            map("n", string.format("<leader>%s", i), function()
+                harpoon:list():select(i)
+            end, { desc = "Harpoon jump to file: [" .. i .. "]" })
+        end
     end,
 }
-
--- lua/user/plugins/harpoon/init.lua
+++ lua/user/plugins/harpoon/init.lua
@@ -1,27 +1,10 @@
-local map = require("core.utils").map
-local harpoon = require "harpoon"
+local M = {}
 
-harpoon:setup({
+M.config = {
     settings = {
         save_on_toggle = true,
         sync_on_ui_close = true,
     },
-})
+}
 
--- Append current open file to our harpon list
-map("n", "<leader>ha", function()
-    harpoon:list():append()
-    local file_name = vim.api.nvim_buf_get_name(0)
-    vim.notify("[Harpoon] Added " .. file_name .. " to list", vim.log.levels.INFO)
-end, { desc = "Harpoon Add" })
-
--- Toggle a UI popup of or current harpooned items.
-map("n", "<leader>hl", function()
-    harpoon.ui:toggle_quick_menu(harpoon:list())
-end, { desc = "Harpoon Toggle" })
-
-for i = 1, 5 do
-    map("n", string.format("<leader>%s", i), function()
-        harpoon:list():select(i)
-    end, { desc = "Harpoon jump to file: [" .. i .. "]" })
-end
+return M
