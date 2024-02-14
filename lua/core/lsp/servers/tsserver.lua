00e5f83f217fb01000042bd0792f2fc79333f63a
justbarnt <functnal@gmail.com>
2024-02-13 20:31:30 -0500

    updated: tsserver specs

--- lua/core/lsp/servers/tsserver.lua
+++ lua/core/lsp/servers/tsserver.lua
@@ -9,26 +9,32 @@ return {
 	},
 	settings = {
 		javascript = {
-			inlayHints = {
-				includeInlayEnumMemberValueHints = true,
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayFunctionParameterTypeHints = true,
-				includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayVariableTypeHints = true,
+			inlayhints = {
+                includeinlayparameternamehints = "all", -- 'none' | 'literals' | 'all';
+                includeinlayparameternamehintswhenargumentmatchesname = false,
+                includeinlayfunctionparametertypehints = true,
+                includeinlayvariabletypehints = false,
+                includeinlaypropertydeclarationtypehints = true,
+                includeinlayfunctionlikereturntypehints = false,
+				includeinlayenummembervaluehints = true,
 			},
+            suggest = {
+                includecompletionsformoduleexports = true,
+            }
 		},
 		typescript = {
-			inlayHints = {
-				includeInlayEnumMemberValueHints = true,
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayFunctionParameterTypeHints = true,
-				includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayVariableTypeHints = true,
+			inlayhints = {
+                includeinlayparameternamehints = "all", -- 'none' | 'literals' | 'all';
+                includeinlayparameternamehintswhenargumentmatchesname = false,
+                includeinlayfunctionparametertypehints = true,
+                includeinlayvariabletypehints = false,
+                includeinlaypropertydeclarationtypehints = true,
+                includeinlayfunctionlikereturntypehints = false,
+				includeinlayenummembervaluehints = true,
 			},
+            suggest = {
+                includecompletionsformoduleexports = true,
+            }
 		},
 	},
 }
