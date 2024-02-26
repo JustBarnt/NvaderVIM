9a96913edd1c7d3841160823e7369caea4acfe8f
justbarnt <functnal@gmail.com>
2024-02-26 18:42:21 -0500

    Added Project setup

--- lua/plugins/projects.lua
+++ lua/plugins/projects.lua
@@ -2,9 +2,8 @@ return {
     "LintaoAmons/cd-project.nvim",
     config = function()
         local project = require 'cd-project'
-        local exists, user_config = pcall(require, 'user.config')
-        local config = exists and type(user_config) == "table" and user_config.project or {}
-    
-
+        local config = require 'user.plugins.projects'
+        
+        project.setup(config)
     end
 }
