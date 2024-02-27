95c072977308b442934e0380a0a41ec9c96a9fd4
justbarnt <functnal@gmail.com>
2024-02-27 16:02:38 -0500

    fixed broken dep for undotree

--- init.lua
+++ init.lua
@@ -7,6 +7,13 @@ if not vim.g.vscode then
     require "core"
     if vim.g.loaded_undotree then
         vim.g.undotree_WindowLayout = 2
+
+        local os_name = jit.os
+        if os_name == 'Windows' then
+            vim.g.undotree_DiffCommand="FC"
+        elseif os_name == 'Linux' then
+            vim.g.undotree_DiffCommand="diff"
+        end
     end
 else
     require("core.keymaps").Default()
