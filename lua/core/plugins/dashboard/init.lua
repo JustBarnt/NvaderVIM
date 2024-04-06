6b854e8dce1afc224cf34d7f16fc9ba54698cd69
justbarnt <functnal@gmail.com>
2024-04-06 10:29:49 -0400

    feat: Renamed Distro

--- lua/core/plugins/dashboard/init.lua
+++ lua/core/plugins/dashboard/init.lua
@@ -4,14 +4,13 @@ return {
     event = "VimEnter",
     config = function()
       local logo = [[
-███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
-            ]]
-
+███╗   ██╗██╗   ██╗ █████╗ ██████╗ ███████╗██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██║   ██║██╔══██╗██╔══██╗██╔════╝██╔══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║██║   ██║███████║██║  ██║█████╗  ██████╔╝██║   ██║██║██╔████╔██║
+██║╚██╗██║╚██╗ ██╔╝██╔══██║██║  ██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║ ╚████╔╝ ██║  ██║██████╔╝███████╗██║  ██║ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝  ╚═══╝  ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+    ]]
       logo = string.rep("\n", 8) .. logo .. "\n\n"
 
       local opts = {
@@ -26,13 +25,6 @@ return {
             { action = "Telescope find_files", desc = " Find file w/Current Directory", icon = " ", key = "f" },
             { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
             { action = "Telescope oldfiles only_cwd=true", desc = " Recent files", icon = " ", key = "r" },
-            { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
-            {
-              action = [[lua require('telescope.builtin').find_files({cwd = vim.fn.stdpath('config')})]],
-              desc = " Config",
-              icon = " ",
-              key = "c",
-            },
             { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
             { action = "Mason", desc = " Mason", icon = "󰒋 ", key = "m" },
             { action = "qa", desc = " Quit", icon = " ", key = "q" },
