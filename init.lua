1a9f33684e68ed51012c2953192168ece44a2dab
bwilliams <bwilliams@commsys.com>
2024-04-09 16:59:48 -0400

    update: restructured init setup

--- init.lua
+++ init.lua
@@ -1,20 +1,20 @@
 -- Mapleader should be set as quick as possible. There is no reason to not set it at the very top of your initial init.lua
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+if vim.g.vscode then
+  return "core.keymaps"
+end
+
 -- Disable netrw
 vim.g.loaded_netrwPlugin = 1
 
-if not vim.g.vscode then
-  require "core"
-  if vim.fn.has "win32" == 1 then
-    local ori_fnameescape = vim.fn.fnameescape
-    ---@diagnostic disable-next-line: duplicate-set-field
-    vim.fn.fnameescape = function(...)
-      local result = ori_fnameescape(...)
-      return result:gsub("\\", "/")
-    end
+require "core"
+if vim.fn.has "win32" == 1 then
+  local ori_fnameescape = vim.fn.fnameescape
+  ---@diagnostic disable-next-line: duplicate-set-field
+  vim.fn.fnameescape = function(...)
+    local result = ori_fnameescape(...)
+    return result:gsub("\\", "/")
   end
-else
-  require "core.keymaps"
-  return
 end
