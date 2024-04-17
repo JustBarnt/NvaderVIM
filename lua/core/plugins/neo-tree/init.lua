7e3b8b2adc918278da47867ccaa8f7c4a0804a4f
bwilliams <bwilliams@commsys.com>
2024-04-17 11:29:06 -0400

    update: Neotree - Navigate through the nodes using `h/l`

--- lua/core/plugins/neo-tree/init.lua
+++ lua/core/plugins/neo-tree/init.lua
@@ -38,6 +38,28 @@ return {
     },
     window = {
       mappings = {
+        ["h"] = function(state)
+          local node = state.tree:get_node()
+          if node.type == "directory" and node:is_expanded() then
+            require("neo-tree.sources.filesystem").toggle_directory(state, node)
+          else
+            require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+          end
+        end,
+        ["l"] = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          if node.type == "directory" then
+            if not node:is_expanded() then
+              require("neo-tree.sources.filesystem").toggle_directory(state, node)
+            elseif node:has_children() then
+              require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+            end
+          end
+          if node.type == "file" then
+            require("neo-tree.utils").open_file(state, path)
+          end
+        end,
         ["df"] = { "diff_files", desc = "Diff Two Files", nowait = true },
         ["q"] = { "close_window", desc = "Close Neo-Tree" },
         ["R"] = { "refresh", desc = "Refresh Neo-Tree Window" },
