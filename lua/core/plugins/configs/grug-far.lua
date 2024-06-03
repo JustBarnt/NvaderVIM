881e8442d27a531097939444cabf0bf833204c78
justbarnt <functnal@gmail.com>
2024-06-03 14:25:10 -0400

    update(added keymap to start Grug)

--- lua/core/plugins/configs/grug-far.lua
+++ lua/core/plugins/configs/grug-far.lua
@@ -1,3 +1,4 @@
+local map = require("core.utils").map
 local has_grug, grug = pcall(require, "grug-far")
 
 if not has_grug then
@@ -6,3 +7,7 @@ if not has_grug then
 end
 
 grug.setup {}
+
+map("n", "<leader>fr", function()
+  require("grug-far").grug_far()
+end, { desc = "Find and Replace with Grug" })
