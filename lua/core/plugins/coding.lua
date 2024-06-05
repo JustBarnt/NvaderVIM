ab280be36a503cd097351f6bee622b4cc8e147de
justbarnt <functnal@gmail.com>
2024-06-05 12:15:16 -0400

    revert(Re-added Comment.nvim, some languages just do not work that well with built in commenting)

--- lua/core/plugins/coding.lua
+++ lua/core/plugins/coding.lua
@@ -27,8 +27,19 @@ return {
     end,
   },
   {
-    "folke/ts-comments.nvim",
-    event = "VeryLazy",
+    "numToStr/Comment.nvim",
+    event = "BufReadPre",
+    keys = {
+      { "gc", mode = { "n", "v" }, desc = "Toggle Comment Line Wise" },
+      { "gb", mode = { "n", "v" }, desc = "Toggle Comment Block Wise" },
+    },
+    opts = function()
+      local commentstring_avail, comment_string = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+      return commentstring_avail and comment_string and { pre_hook = comment_string.create_pre_hook() } or {}
+    end,
+    config = function(_, opts)
+      require("Comment").setup(opts)
+    end,
   },
   {
     "mfussenegger/nvim-dap",
