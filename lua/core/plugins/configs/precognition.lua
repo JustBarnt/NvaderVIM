13638044c51ea3f0b28f101bde512cfddeb50c4f
justbarnt <functnal@gmail.com>
2024-06-11 12:23:02 -0400

    update(update precognition. Toggle maps to `<leader>pt`)

--- lua/core/plugins/configs/precognition.lua
+++ lua/core/plugins/configs/precognition.lua
@@ -5,4 +5,12 @@ if not has_precog then
   return
 end
 
-precognition.setup({})
+local map = require("core.utils").map
+
+precognition.setup({
+  startVisible = true,
+  showBlankVirtLine = false,
+  highlightColor = { link = "@constant" },
+})
+
+map("n", "<leader>pt", "<CMD>Precognition toggle<CR>", { desc = "Toggle Precognition" })
