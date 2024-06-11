cd78dc8be34265e02b85e27415fc739e1e9ec1f3
justbarnt <functnal@gmail.com>
2024-06-11 13:32:07 -0400

    updated nordic to not use italics

--- lua/core/plugins/configs/themes/nordic.lua
+++ lua/core/plugins/configs/themes/nordic.lua
@@ -5,56 +5,11 @@ if not has_nordic then
   return
 end
 
---[[
-black0 = '#191D24',
-black1 = '#1E222A',
-black2 = '#222630',
-gray0 = '#242933', -- bg
-gray1 = '#2E3440',
-gray2 = '#3B4252',
-gray3 = '#434C5E',
-gray4 = '#4C566A',
-gray5 = '#60728A',
-white0_normal = '#BBC3D4',
-white0_reduce_blue = '#C0C8D8',
-white1 = '#D8DEE9',
-white2 = '#E5E9F0',
-white3 = '#ECEFF4',
-blue0 = '#5E81AC',
-blue1 = '#81A1C1',
-blue2 = '#88C0D0',
-
-cyan
-    base = '#8FBCBB',
-    bright = '#9FC6C5',
-    dim = '#80B3B2',
-red
-    base = '#BF616A',
-    bright = '#C5727A',
-    dim = '#B74E58',
-orange
-    base = '#D08770',
-    bright = '#D79784',
-    dim = '#CB775D',
-yellow
-    base = '#EBCB8B',
-    bright = '#EFD49F',
-    dim = '#E7C173',
-green
-    base = '#A3BE8C',
-    bright = '#B1C89D',
-    dim = '#97B67C',
-magenta
-    base = '#B48EAD',
-    bright = '#BE9DB8',
-    dim = '#A97EA1',
-]]
-
 local C = require("nordic.colors")
 
 nordic.setup({
   bold_keywords = true,
-  italic_comments = true,
+  italic_comments = false,
   transparent_bg = true,
   bright_border = true,
   reduced_blue = true,
