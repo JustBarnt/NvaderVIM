48441ad0efc0d257959ba5848b11c1051cf478c5
bwilliams <bwilliams@commsys.com>
2024-07-02 09:06:01 -0400

    updated oil based on new changes

--- lua/core/plugins/editor.lua
+++ lua/core/plugins/editor.lua
@@ -183,7 +183,7 @@ return {
         },
         skip_confirm_for_simple_edits = true,
         prompt_save_on_select_new_entry = false,
-        experimental_watch_for_changes = true,
+        watch_for_changes = true,
         view_options = {
           is_always_hidden = function(name, bufnr)
             local ignore = { ".sveltekit", "build", "node_modules" }
